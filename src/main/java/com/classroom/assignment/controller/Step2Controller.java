f85b10d86efbfc803bb0ff9a25aa18c1bf24be9e
st323a2107k-kakimoto <146682503+st323a2107k-kakimoto@users.noreply.github.com>
2023-10-03 08:46:17 +0000

    Co-authored-by: st323a2117d-taniguchi <st323a2117d-taniguchi@users.noreply.github.com> Co-authored-by: st323a2115h-takeuchi <st323a2115h-takeuchi@users.noreply.github.com> Co-authored-by: st323a2106k-okuyama <st323a2106k-okuyama@users.noreply.github.com> Co-authored-by: st323a2122k-tozaki <st323a2122k-tozaki@users.noreply.github.com>

--- src/main/java/com/classroom/assignment/controller/Step2Controller.java
+++ src/main/java/com/classroom/assignment/controller/Step2Controller.java
@@ -18,17 +18,17 @@ public class Step2Controller {
   // 例
   @GetMapping("/morning")
   // nameというパラメータ名で値を指定し、Getパラメータを必須にしない為に「name = "name", required = false」と指定する。
-  public String morning(@RequestParam(name = "name", required = false) String name, Model model) {
+  public String morning(@RequestParam(name = "name", required = false) String name, String string) {
     model.addAttribute("greeting", name + "さん、おはようございます!");
     return "step2/greeting";
   }
 
   // 課題 level1 step2
-  @GetMapping("書き換えてください")
+  @GetMapping("/evening")
   public String evening(@RequestParam(name = "name", required = false) String name, Model model) {
     // 「/step2/evening?name=○○」をURLの最後に指定した時に「○○さん、こんばんは!」と返すよう処理を追加してください。
     // ↓↓↓↓ここに処理を追加しましょう。↓↓↓↓
-
+    model.addAttribute("greeting", name + "さん、こんばんは!");
     // ↑↑↑↑ここに処理を追加しましょう。"↑↑↑↑
     return "step2/greeting";
   }
